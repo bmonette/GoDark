3500b5d192fa16fab1bf59e3afc5836b9cbb67ad
bmonette <benoitmonette80@gmail.com>
2025-07-07 11:30:45 -0400

    Updated the logic

--- background.js
+++ background.js
@@ -1,6 +1,20 @@
-chrome.action.onClicked.addListener((tab) => {
+chrome.action.onClicked.addListener(async (tab) => {
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      const marker = document.documentElement.getAttribute('data-godark');
+
+      if (marker === 'enabled') {
+        location.reload();
+      } else {
+        document.documentElement.setAttribute('data-godark', 'enabled');
+      }
+    }
+  });
+
+  // Inject CSS only if not already active
   chrome.scripting.insertCSS({
     target: { tabId: tab.id },
-    files: ["dark-mode.css"]
+    files: ['dark-mode.css']
   });
 });
